0b097fb7853ce19caa3e3e6b864fb3b677b9849a
ismail taibi <61507239+ismadevjs@users.noreply.github.com>
2021-05-27 13:33:12 -0700

    init project
    tomorrow will be my start of my new learning path. 28-05-2021

---
+++ router.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const path = require("path");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname)); //Appending extension
+  },
+});
+const upload = multer({ storage: storage });
+// frontend area
+module.exports = router;
