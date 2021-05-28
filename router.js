69461fc8c2fd0f28b9c7297f6161b38505c31f95
ismail taibi <61507239+ismadevjs@users.noreply.github.com>
2021-05-28 04:43:35 -0700

    set up sognup page

--- router.js
+++ router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const multer = require("multer");
 const path = require("path");
 const specialPages = require("./controller/specialPages");
+const frontendController = require("./controller/frontendController");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "uploads/");
@@ -16,6 +17,8 @@ const upload = multer({ storage: storage });
 router.get("/", function (req, res) {
   res.send("hello from github");
 });
+
+router.get("/signup", frontendController.signup);
 router.get("/404", specialPages.errorPage);
 
 module.exports = router;
